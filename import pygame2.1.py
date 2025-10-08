a3e287281ee10352f2cf2ae9bb6740de8716c61c
subnerf <klasendaniel8@gmail.com>
2025-10-08 14:48:58 -0600

    Fix player movement logic by adding missing line for 'A' key handling

--- import pygame2.1.py
+++ import pygame2.1.py
@@ -2,19 +2,21 @@ import pygame
 
 #setup
 pygame.init()
-screen = pygame.display.set_mode((800,500))
+WINDOW_WIDTH = 800
+WINDOW_HEIGHT = 500
+screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 pygame.display.set_caption("made by subnerf2")
 clock = pygame.time.Clock()
 running = True
 
 dt = 0
-player_pos = pygame.Vector2(screen.get_width()/2, screen.get_height()/2)
+player_pos = pygame.Vector2(WINDOW_WIDTH/2, WINDOW_HEIGHT/2)
 
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-# Input handling
+
     keys = pygame.key.get_pressed()
     if keys[pygame.K_w]:
         player_pos.y -= 300 * dt
@@ -22,19 +24,22 @@ while running:
         player_pos.y += 300 * dt
     if keys[pygame.K_a]:
         player_pos.x -= 300 * dt
-
     if keys[pygame.K_d]:
         player_pos.x += 300 * dt 
 
-    if event.type == pygame.KEYDOWN:
-        if event.key == pygame.MOUSEBUTTONUP:
+    if event.type == pygame.MOUSEBUTTONDOWN:
+        pos = pygame.mouse.get_pos()
+        player_pos.x = pos[0]
+        player_pos.y = pos[1]
+    
+    if event.type == pygame.MOUSEBUTTONUP:
             pos = pygame.mouse.get_pos()
-        print(pos)
-
+            pygame.draw.circle(screen, "#000000", player_pos, 40)
+        
     dt = clock.tick(60) / 1000  # Limit to 60 FPS and get delta time
 
 
-    screen.fill("#000000")
+    screen.fill("#FFFFFF")
     pygame.draw.circle(screen, (200,50,50), player_pos, 25)
     pygame.display.flip()
 
